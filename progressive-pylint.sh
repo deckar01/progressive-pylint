87e2b4ee8683d490df267f2545dae03a7b5bdb2b
Jared Deckard <jared.deckard@gmail.com>
2016-11-30 09:01:38 -0600

    Add a bash script to run pylint progressively by category

---
+++ progressive-pylint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a names=("errors" "warnings" "refactors" "conventions")
+declare -a codes=("W,R,C" "E,R,C" "E,W,C" "E,R,W")
+
+for (( i=0; i<${#names[@]}; i++ )); do
+  echo "Looking for ${names[$i]}..."
+  log=$(pylint --disable=${codes[i]} $@ 2>&1)
+
+  # If there were issues, show them and stop.
+  if [ $? -ne 0 ]; then
+    echo "$log"
+    return $status
+  fi
+done
+
+echo "Lint free!"
